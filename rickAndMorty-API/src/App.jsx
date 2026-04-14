0b3e44f086adc03ae54f13afd0af5839ab3b2e45
L3ticias0uz4 <leticiasouza56000@gmail.com>
2026-04-14 11:00:38 -0300

    Updating API CSS

--- rickAndMorty-API/src/App.jsx
+++ rickAndMorty-API/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import { Header } from './components/Header' 
 import {Main} from './components/Main'
-import {Card} from './components/Card'
 import { SectionCharacters } from './components/SectionCharacters'
 function App() {
  
